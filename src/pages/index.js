b7198d39c71bc8ee0b81d7010ba8b846ca4a52a4
mai <maicmi@me.com>
2022-02-15 22:04:23 +0700

    playsInline for react

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ const IndexPage = () => {
     <div className="overflow-wrap">
     <section id="home" className="flex section--dark">
         <div className="bg-video">
-            <video className="bg-video__content" autoPlay={true} loop muted playsinline>
+            <video className="bg-video__content" autoPlay={true} loop muted playsInline>
                 <source src={bgvdo} type="video/mp4" />
             </video>
         </div>
