1de50d0c827efdf523bc91ac398ac4ca54ad5314
openmymai <maicmi@me.com>
2022-11-24 21:14:09 +0700

    Update Lesson0408 More

---
+++ src/pages/lessonspecial.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useRef } from 'react'
+import { navigate } from 'gatsby'
+import Typed from 'typed.js'
+import bgvdo from '../videos/mountain.mp4'
+import '../scss/main.scss'
+
+
+// markup
+const LessonSpecial = () => {
+
+  const el = useRef(null)
+  const typed = useRef(null)
+  useEffect(() => {
+    const options = {
+      strings: [
+        `ตาเห็นรูป`,
+        `รูปไม่เที่ยงเกิดดับ`,
+        `ตัวฉันไม่เที่ยงเกิดดับ`,
+        `หูได้ยินเสียง`,
+        `เสียงไม่เที่ยงเกิดดับ`,
+        `ตัวฉันไม่เที่ยงเกิดดับ`,
+        `จมูกได้กลิ่น`,
+        `กลิ่นไม่เที่ยงเกิดดับ`,
+        `ตัวฉันไม่เที่ยงเกิดดับ`,
+        `ลิ้นได้รส`,
+        `รสไม่เที่ยงเกิดดับ`,
+        `ตัวฉันไม่เที่ยงเกิดดับ`,
+        `กายสัมผัส`,
+        `ไม่เที่ยงเกิดดับ`,
+        `ตัวฉันไม่เที่ยงเกิดดับ`,
+        `ใจคิดนึก`,
+        `ไม่เที่ยงเกิดดับ`,
+        `ตัวฉันไม่เที่ยงเกิดดับ`,
+        ],
+      typeSpeed: 60,
+      fadeOut: true,
+      fadeOutDelay: 0,
+      loop: true,
+    };
+    typed.current = new Typed(el.current, options)
+
+    return () => {
+      typed.current.destroy()
+    }
+  }, [])
+  return (
+    <>
+      <div className="overflow-wrap">
+      <section id="home" className="flex section--dark">
+          <div className="bg-video">
+          <video className="bg-video__content" autoPlay={true} loop muted playsInline>
+                  <source src={bgvdo} type="video/mp4" />
+              </video>
+          </div>
+          <div className="flex">
+              <h1>
+                <div className="text">
+                  <span style={{ whiteSpace: 'pre' }} ref={el} />
+                </div>
+              </h1>        
+          </div>
+      </section>
+      </div>
+    </>
+  )
+}
+
+export default LessonSpecial
